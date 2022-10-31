ab0650b1cdaa0b708d38e81e34be474c4c9bfa15
JanineTeodoro <janineteodoro@hotmail.com>
2022-10-30 22:38:17 -0300

    Alguns dos ícones adicionados

--- src/components/barra-inferior.js
+++ src/components/barra-inferior.js
@@ -1,14 +1,18 @@
 import React from "react"
-import {Text, StyleSheet, View, Button, Dimensions} from "react-native"
-
+import {Text, StyleSheet, View, Button, Dimensions, TouchableHighlight, Image} from "react-native"
 export const BarraInferior = () => (
     <View
         style={{...styles.BarraInferior, ...styles.button}}
     >
-        <Button
-            title="Home"
-            color="#7fc7bd"
-        />
+        <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#7fc7bd"
+            onPress={() => alert('Pressed!')}>
+            <Image 
+            source={require("../../assets/home.png")} 
+            style={styles.imageButton}
+            />
+        </TouchableHighlight>
         <Button
             title="Plantões"
             color="#7fc7bd"
@@ -17,10 +21,15 @@ export const BarraInferior = () => (
             title="Favoritos"
             color="#7fc7bd"
         />
-        <Button
-            title="Minha conta"
-            color="#7fc7bd"
-        />
+        <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#7fc7bd"
+            onPress={() => alert('Pressed!')}>
+            <Image 
+            source={require("../../assets/seta.png")} 
+            style={styles.imageButton}
+            />
+        </TouchableHighlight>
     </View>
 )
 
@@ -32,5 +41,10 @@ const styles = StyleSheet.create({
     },
     button:{
         alignSelf: "stretch"
+    },
+    imageButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#7fc7bd'
     }
 })
--- src/components/barra-superior.js
+++ src/components/barra-superior.js
@@ -1,13 +1,18 @@
 import React from "react"
-import {Text, StyleSheet, View, Button, Dimensions} from "react-native"
+import {Text, StyleSheet, View, Dimensions, TouchableHighlight, Image} from "react-native"
 
 export const BarraSuperior = () => (
     <View
         style={styles.BarraSuperior}>
-        <Button
-            title="Voltar"
-            color="red"
+        <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#7fc7bd"
+            onPress={() => alert('Pressed!')}>
+            <Image 
+            source={require("../../assets/seta.png")} 
+            style={styles.imageButton}
             />
+        </TouchableHighlight>
         <Text>Plantões</Text>
     </View>
 )
@@ -18,5 +23,10 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-around",
         width: Dimensions.get("window").width
+    },
+    imageButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#7fc7bd'
     }
 })
