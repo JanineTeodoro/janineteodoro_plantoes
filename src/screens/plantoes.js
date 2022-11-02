e54b83ab3b3d0bbfe4a65335e68bf8b15aa8a1b2
JanineTeodoro <janineteodoro@hotmail.com>
2022-11-02 15:44:56 -0300

    Todos os componentes foram adicionados e cores foram ajustadas

--- src/screens/plantoes.js
+++ src/screens/plantoes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Text, StyleSheet, Image, View, Dimensions} from "react-native"
+import {Text, StyleSheet, Image, View, Dimensions, TouchableOpacity} from "react-native"
 
 export const Plantoes = () => (
     <View>
@@ -18,7 +18,7 @@ export const Plantoes = () => (
             <View style={{flex:1}}>
                 <Image 
                 style={styles.button2}
-                source={require("../../assets/professional.jpg")} 
+                source={require("../../assets/professional.png")} 
                 />
             </View>
             <View style={{flex:3}}>
@@ -26,12 +26,53 @@ export const Plantoes = () => (
                 style={styles.titleText2}>
                 <Text style={{fontWeight: "600"}}>Janine Leite Teodoro</Text>
             </Text>
-            <Text
-                style={styles.titleText3}>
-                <Text style={{fontWeight: "500"}}>Ligar para o profissional</Text>
-            </Text>
+                <View style={styles.ligar}>
+                    <TouchableOpacity
+                        activeOpacity={0.6}
+                        underlayColor="#7fc7bd"
+                        onPress={() => alert('Reportar')}>
+                        <Image 
+                        style={styles.button3}
+                        source={require("../../assets/phone-green.png")} 
+                        />
+                        <Text
+                        style={styles.titleText3}>
+                        <Text style={{fontWeight: "500"}}>Ligar para o profissional</Text>
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
+            <View style={styles.buttonsCenter}>
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    underlayColor="#7fc7bd"
+                    onPress={() => alert('Reportar')}>
+                    <View>
+                    <Image 
+                    source={require("../../assets/alert-red.png")} 
+                    style={styles.imageButton1}
+                    />
+                    <Text
+                        style={styles.textButton1}
+                    >Reportar</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    underlayColor="#7fc7bd"
+                    onPress={() => alert('Avaliar')}>
+                    <View>
+                    <Image 
+                    source={require("../../assets/star-blue.png")} 
+                    style={styles.imageButton2}
+                    />
+                    <Text
+                        style={styles.textButton2}
+                    >Avaliar</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
     </View>
 
 )
@@ -72,21 +113,75 @@ const styles = StyleSheet.create({
         color: "#6d7a78",
         fontWeight: "500",
         fontSize: 20,
-        alignSelf: "center"
+        marginTop: 20,
+        marginLeft: 40
+    },
+    ligar: {
+        width:Dimensions.get("screen").width,
+        marginTop: 15,
+        marginLeft: 40,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    button3: {
+        width: 25,
+        height: 25
     },
     titleText3: {
         color: "#6d7a78",
         fontWeight: "500",
-        color: "green",
+        color: "#32CD32",
         fontSize: 16,
-        alignSelf: "center"
+        marginLeft: 40
     },
     professional: {
         borderTopWidth: 0.5,
-        borderBottomWidth: 0.5,
         borderColor: "#B0BEC5",
         backgroundColor: "white",
         width:Dimensions.get("screen").width,
         flexDirection: "row"
+    },
+    buttonsCenter: {
+        width:Dimensions.get("screen").width,
+        flexDirection: "row",
+        borderBottomWidth: 0.5,
+        borderColor: "#B0BEC5",
+        padding: 10
+    },
+    imageButton1: {
+        width: 30,
+        height: 30,
+        marginTop: 5,
+        alignSelf: "center",
+        marginLeft: 110
+    },
+    imageButton2: {
+        width: 30,
+        height: 30,
+        marginTop: 5,
+        alignSelf: "center",
+        marginLeft: 30
+    },
+    textButton1: {
+        width: 98,
+        height: 40,
+        marginBottom: -15,
+        textAlign: "center",
+        fontSize: 14,
+        fontWeight: "700",
+        color: "red",
+        marginLeft: 110,
+        marginBottom: 2
+    },
+    textButton2: {
+        width: 98,
+        height: 40,
+        marginBottom: -15,
+        textAlign: "center",
+        fontSize: 14,
+        fontWeight: "700",
+        color: "#1E90FF",
+        marginLeft: 30,
+        marginBottom: 2
     }
 })
